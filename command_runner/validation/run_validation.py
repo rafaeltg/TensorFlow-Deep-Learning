fe31e277e2b0c4afa109f2b5f769380ecc84891b
Rafael Thomazi Gonzalez <rthomazigonzalez@gmail.com>
2016-09-13 16:46:43 -0300

    Update run_validation.py

--- command_runner/validation/run_validation.py
+++ command_runner/validation/run_validation.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-
 import models.autoencoder_models as ae_models
 import models.nnet_models as nn_models
+from utils import datasets
 from validator.model_validator import ModelValidator
 
 # #################### #
@@ -11,11 +11,47 @@ from validator.model_validator import ModelValidator
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('model', 'mlp', 'Model to validate.')
 flags.DEFINE_string('method', 'split', 'Validation method.')
 flags.DEFINE_integer('n_folds', 10, 'Number of cv folds.')
 flags.DEFINE_float('test_size', 0.3, 'Percentage of data used for validation.')
 flags.DEFINE_string('dataset_x', '', 'Path to the dataset file (.npy or .csv).')
 flags.DEFINE_string('dataset_y', '', 'Path to the dataset outputs file (.npy or .csv).')
+flags.DEFINE_string('metrics', 'mse,mae,rmse', '')
 
 
+params = {
+  'method':    FLAGS.method,
+  'k':         FLAGS.n_folds,
+  'test_size': FLAGS.test_size,
+  'dataset_x': FLAGS.dataset_x,
+  'dataset_y': FLAGS.dataset_y
+  'metrics':   utils.flag_to_list(FLAGS.metrics, 'str'),
+}
+
+
+func run_validation(model, **kwargs) {
+  
+  validador = ModelValidator(method=kwargs.get('method'),
+                             k=kwarg.get('k'),
+                             test_size=kwargs.get('test_size'))
+  
+  res = validator.run(model=model,
+                      x=kwargs.get('dataset_x'),
+                      y=kwargs.get('dataset_y'),
+                      metrics=kwargs.get('metrics'))
+  
+  print('----------------------------------')
+  print('        VALIDATION RESULTS        ')
+  
+  print('> Test score = %f (std dev = %f)' % (res['scores'].mean(), res['scores'].sd()))
+  
+  for m, v in enumerate(res['metrics']):
+    print('> %s = %f (std dev = %f)' % (m, v.mean(), v.sd()))
+}
+
+
+if __name__ == '__main__':
+  
+  model = nn_models.MLP() # default parameters
+  
+  run_validation(model, **params)
