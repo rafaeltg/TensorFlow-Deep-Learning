8223c364a37b00d10f0c05baad2e31be1dc51698
Rafael Thomazi Gonzalez <rthomazigonzalez@gmail.com>
2016-09-13 17:00:09 -0300

    Update run_validation.py

--- command_runner/validation/run_validation.py
+++ command_runner/validation/run_validation.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import models.autoencoder_models as ae_models
 import models.nnet_models as nn_models
 from utils import datasets
@@ -43,10 +44,10 @@ func run_validation(model, **kwargs) {
   print('----------------------------------')
   print('        VALIDATION RESULTS        ')
   
-  print('> Test score = %f (std dev = %f)' % (res['scores'].mean(), res['scores'].sd()))
+  print('> Test score = %f (std dev = %f)' % (np.mean(res['scores']), np.std(res['scores'])))
   
   for m, v in enumerate(res['metrics']):
-    print('> %s = %f (std dev = %f)' % (m, v.mean(), v.sd()))
+    print('> %s = %f (std dev = %f)' % (m, np.mean(v), np.std(v)))
 }
 
 
@@ -54,4 +55,6 @@ if __name__ == '__main__':
   
   model = nn_models.MLP() # default parameters
   
-  run_validation(model, **params)
+  dataset = datasets.load_dataset(params['dataset_x'], params['dataset_y'])
+  
+  run_validation(model, dataset.data, dataset.target, **params)
